4cab55c169f94914931481d66d13b05212fefcc2
Tobias <tobias@tobiasrijken.com>
2014-11-20 18:16:05 +0000

    added method to list and load json files

--- assignment2.py
+++ assignment2.py
@@ -1,15 +1,30 @@
-import nltk
+import glob
 import json
+import nltk
+from pprint import pprint
 
 """
 TO DO
-- Read Data
 - creat document classes
 - implement Naive Bayes
 """
 
+# Create a list of .json file names
+def listFiles(path="./bionlp2011genia-train-clean/*.json"):
+	return glob.glob(path)
+
+# Opens and loads a single json file name and returns it
+def loadJSONfile(fileName):
+	with open(fileName) as rawJSON:
+		d = json.load(rawJSON)
+		return d
+
 def main():
-	pass
+	# Just test my functions a bit
+	listOfFiles = listFiles()
+	print listOfFiles[0]
+	f1 = loadJSONfile(listOfFiles[0])
+	pprint(f1)
 
 if __name__ == '__main__':
 	main()
