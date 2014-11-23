e899ac2e8b4ad3d1a23a0091cdb0cea9352ad7a0
Michael Michaelides <webdrone@gmail.com>
2014-11-23 16:17:32 +0000

    Added feature vector functionality by extending dict class. Includes alternative implementation for contingency.

--- assignment2.py
+++ assignment2.py
@@ -2,6 +2,7 @@ import glob
 import json
 import nltk
 from pprint import pprint
+import feature_vector
 
 """
 TO DO
@@ -19,12 +20,20 @@ def loadJSONfile(fileName):
 		d = json.load(rawJSON)
 		return d
 
+# Instantiating the feature vector class (creating a dict of feature vectors)
+f_v = feature_vector.FeatureVector()
+
 def main():
 	# Just testing my functions a bit
 	listOfFiles = listFiles()
 	print (listOfFiles[0])
 	f1 = loadJSONfile(listOfFiles[0])
 	pprint(len(f1['sentences']))
+
+	# Testing code Feature Vector functionality
+	f_v[('test','Regulation')] =1
+	print (f_v)
+	print (f_v['missing key'])
 
 if __name__ == '__main__':
 	main()
--- feature_vector.py
+++ feature_vector.py
@@ -1,7 +1,32 @@
-'''
+"""
 Class template for feature vectors. f(x,c)
-'''
-class FeatureVector(Object):
-    def __init__(self):
-        ...
+Extending the dictionary class to return 0 if one tries to acceess a feature vector for a missing key.
+An alternative implementation is commented below in case the method of extending the dict class is not adequate.
+"""
+class FeatureVector(dict):
+    def __missing__(self, key):
+        return 0
 
+# # testing code
+# feature_vector = FeatureVector()
+# feature_vector[('test','Regulation')] =1
+# print (feature_vector)
+# # {('test', 'Regulation'): 1}
+# print (feature_vector['not there'])
+# # 0
+
+"""
+class FeatureVector():
+    feature_vectors = {}
+    def __init__(cls, word_c, weight=1.0):
+        cls.feature_vectors[word_c] = weight
+
+    @classmethod
+    def get_all(cls):
+        return cls.feature_vectors
+
+# testing code
+FeatureVector(('test','Regulation'))
+print (FeatureVector.get_all())
+# Output: {('test', 'Regulation'): 1.0}
+"""
