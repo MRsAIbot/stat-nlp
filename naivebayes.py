f3cbac1fd97d9fcfbb18ec3442c086b56c651e33
Tobias <tobias@tobiasrijken.com>
2014-11-23 00:51:58 +0000

    initial commit for naive bayes implementation

---
+++ naivebayes.py
@@ -0,0 +1,10 @@
+"""
+@author: Tobias Rijken
+"""
+import numpy as np
+
+class NaiveBayes(object):
+	"""docstring for NaiveBayes"""
+	def __init__(self, arg):
+		super(NaiveBayes, self).__init__()
+		self.arg = arg
