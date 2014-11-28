1bb9178a104b65b1b9d0dd363157694e528fa2ca
Tobias <tobias@tobiasrijken.com>
2014-11-28 11:45:45 +0000

    added smoothing technique

--- naivebayes.py
+++ naivebayes.py
@@ -3,9 +3,10 @@ from scipy.sparse import issparse
 
 class NaiveBayes(object):
 	"""docstring for NaiveBayes"""
-	def __init__(self, arg):
-		super(NaiveBayes, self).__init__()
-		self.arg = arg
+	def __init__(self, fit_prior=None, class_prior=None):
+		self.fit_prior = fit_prior
+		self.class_prior = class_prior
+
 
 	def safe_sparse_dot(a, b, dense_output=False):
 		if issparse(a) or issparse(b):
@@ -14,13 +15,16 @@ class NaiveBayes(object):
 				ret = ret.toarray()
 			return ret
 
+	# An internal function to compute the log likelihood
 	def _joint_log_likelihood(self, X):
 		X = check_array(X, accept_sparse='csr')
 		return (safe_sparse_dot(X, self.feature_log_prob_.T) + self.class_log_prior_)
 
+	# A function that fits our model
 	def fit(self, X, y):
 		pass
 
+	# A function to partially fit our model to enable online learning
 	def fit_online(self, X, y):
 		pass
 
@@ -28,6 +32,14 @@ class NaiveBayes(object):
 		self.feature_count = safe_sparse_dot(y.T, X)
 		self.class_count = y.sum(axis=0)
 
+	# Apply add-K smoothing
+	def _apply_smoothing(self, k=1.0):
+		feature_count_smooth = self.feature_count + a
+		class_count_smooth = feature_count_smooth.sum(axis=1)
+
+		self.feature_log_prob_ = np.log(feature_count_smooth) - np.log(class_count_smooth.reshape(-1,1))
+
+	# Given a new set of datapoints, this function predict the class
 	def predict(self, X):
 		joint_log_likelihood = self._joint_log_likelihood(X)
 		return self.classes_[np.argmax(joint_log_likelihood, axis=1)]
