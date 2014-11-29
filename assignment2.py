ecf030644364be0cd56ceac209b5842f55e07ef4
Tobias <tobias@tobiasrijken.com>
2014-11-29 22:55:50 +0000

    added test code

--- assignment2.py
+++ assignment2.py
@@ -6,8 +6,8 @@ from collections import defaultdict
 from pprint import pprint
 import feature_vector
 import numpy as np
-import naivebayes as nb
-
+import random
+from sklearn.naive_bayes import BernoulliNB
 
 # Create a list of .json file names
 def list_files(path="./bionlp2011genia-train-clean/*.json"):
@@ -44,51 +44,60 @@ def get_all_arguments(file_list):
                 for argument in arguments_list:
                     argument_dict[argument['gold']] += 1
     return argument_dict
-    
 
-    
-    
 
 def main():
-    # Just testing my functions a bit
-    list_of_files = list_files()
-    print (list_of_files[0])
-    f1 = load_json_file(list_of_files[0])
-    pprint(len(f1['sentences']))
-        
-    # Finding and counting all event triggers
-    t = get_all_triggers(list_of_files)
-    print("Number of distinct event triggers: {0}".format(len(t.keys())))
-    pprint(t)
+	# Just testing my functions a bit
+	list_of_files = list_files()
+	print (list_of_files[0])
+	f1 = load_json_file(list_of_files[0])
+	pprint(len(f1['sentences']))
+	    
+	# Finding and counting all event triggers
+	t = get_all_triggers(list_of_files)
+	print("Number of distinct event triggers: {0}".format(len(t.keys())))
+	pprint(t)
 
-	# Finding and counting all possible arguments (=relationship labels)
-	arg = get_all_arguments(list_of_files)
-	print("Number of relation arguments: {0}".format(len(arg.keys())))
-	pprint(arg)
+	# # Finding and counting all possible arguments (=relationship labels)
+	# arg = get_all_arguments(list_of_files)
+	# print("Number of relation arguments: {0}".format(len(arg.keys())))
+	# pprint(arg)
 
-	# Test of sparse vectors
-	list_a = []
-	list_a.append(feature_vector.phi_alternative_0)
-	list_a.append(feature_vector.phi_alternative_1)
+	# # Test of sparse vectors
+	# list_a = []
+	# list_a.append(feature_vector.phi_alternative_0)
+	# list_a.append(feature_vector.phi_alternative_1)
 
-	listOfFiles = list_files()
-	f1 = load_json_file(listOfFiles[0])
-	sentence = f1['sentences'][0]   #pick first sentence
-	token_index = 0 #first word in sentence
+	# listOfFiles = list_files()
+	# f1 = load_json_file(listOfFiles[0])
+	# sentence = f1['sentences'][0]   #pick first sentence
+	# token_index = 0 #first word in sentence
 
-	grammar_dict = feature_vector.identify_all_grammar_tags(listOfFiles)   
-	all_grammar_tags = grammar_dict.keys()  #these lists should be saved and later loaded.
+	# grammar_dict = feature_vector.identify_all_grammar_tags(listOfFiles)   
+	# all_grammar_tags = grammar_dict.keys()  #these lists should be saved and later loaded.
 
-	f_v=feature_vector.FeatureVector(list_a)
-	vec = f_v.get_vector_alternative( token_index, sentence, all_grammar_tags)
+	# f_v=feature_vector.FeatureVector(list_a)
+	# vec = f_v.get_vector_alternative( token_index, sentence, all_grammar_tags)
 
-	if 1:
-         f_matrix = f_v.get_feature_batch( [0,1,2], [sentence]*3, all_grammar_tags)
-         print np.array(f_matrix.todense())
-         print f_matrix.col
-         print f_matrix.row
-         print f_matrix.data
+	# if 1:
+	# 	f_matrix = f_v.get_feature_batch( [0,1,2], [sentence]*3, all_grammar_tags)
+	# 	print np.array(f_matrix.todense())
+	# 	print f_matrix.col
+	# 	print f_matrix.row
+	# 	print f_matrix.data
 
+	# Test Naive Bayes
+	X = np.random.randint(2, size=(20,100))
+	print X
+	y = np.array([t.keys()[i] for i in [random.randint(0,9) for p in range(20)]])
+	print y
+	# NB = nbclf.NaiveBayes()
+	# NB.train(X,y)
+	# print(NB.predict(X[2]))
+
+	clf = BernoulliNB()
+	clf.fit(X,y)
+	print(clf.predict(X[2]))
 
 
 if __name__ == '__main__':
